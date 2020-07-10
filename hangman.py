08e24384d89f34a101968a0c7306ab8c2883df23
Edwin Mwaura <mwauraedwin97@gmail.com>
2020-07-10 19:24:12 +0300

    Included letter guessing for eight tries, results not yet included

--- hangman.py
+++ hangman.py
@@ -6,16 +6,25 @@ random_choice = random.choice(my_list)
 
 num = len(random_choice)
 
-dashes = "-" * (num)
-print(dashes)
+dashes = "-" * num
+dash_list = list(dashes)
 
 hidden_word = random_choice[:3:] + dashes
 
-print("H A N G M A N")
-print(dashes)
-word = input()
+print("H A N G M A N\n")
 
-if word == random_choice:
-    print("You survived!")
-else:
-    print("You are hanged!")
+tries = 0
+while tries < 8:
+    print("".join(dash_list))
+    letter = input("Input a letter: ")
+    if letter in random_choice:
+        for i in range(0, num):
+            if random_choice[i] == letter:
+                dash_list[i] = random_choice[i]
+        print()
+    else:
+        print("No such letter in the word\n")
+    tries += 1
+
+print("Thanks for playing!")
+print("We'll see how well you did in the next stage")
