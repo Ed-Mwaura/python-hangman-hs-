3112e65f226ae0323e2bf3d9b8b33243c622657e
Edwin Mwaura <mwauraedwin97@gmail.com>
2020-07-13 14:53:15 +0300

    Improved score calculation, repeated letters no longer reduce score

--- hangman.py
+++ hangman.py
@@ -6,6 +6,9 @@ random_choice = random.choice(my_list)
 
 num = len(random_choice)
 
+ascii_lowercase = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                   'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
 dashes = "-" * num
 dash_list = list(dashes)
 
@@ -23,28 +26,32 @@ while tries > 0:
         break
 
     letter = input("Input a letter: ")
-    if letter in random_choice:
-        if letter not in chosen_letter:
-            print()
-            for i in range(0, num):
-                if random_choice[i] == letter:
-                    dash_list[i] = random_choice[i]
-                    chosen_letter.add(letter)
-
-        else:
-            print("No improvements")
-            tries -= 1
-            if tries == 0:
-                print("You are hanged!")
-            else:
-                print()
-
+    if len(letter) != 1:
+        print("You should input a single letter\n")
     else:
-        print("No such letter in the word")
-        tries -= 1
-        if tries == 0:
-            print("You are hanged!")
+        if letter not in ascii_lowercase:
+            print("It is not an ASCII lowercase letter\n")
+
         else:
-            print()
+            if letter in random_choice:
+                if letter not in chosen_letter:
+                    print()
+                    for i in range(0, num):
+                        if random_choice[i] == letter:
+                            dash_list[i] = random_choice[i]
+                            chosen_letter.add(letter)
 
+                else:
+                    print("You already typed this letter\n")
 
+            else:
+                if letter in chosen_letter:
+                    print("You already typed this letter\n")
+                else:
+                    print("No such letter in the word")
+                    chosen_letter.add(letter)
+                    tries -= 1
+                    if tries == 0:
+                        print("You are hanged!")
+                    else:
+                        print()
