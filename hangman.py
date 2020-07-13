6f87b9a90a67eed3b8f1b27424b4b272b65746cc
Edwin Mwaura <mwauraedwin97@gmail.com>
2020-07-13 15:23:59 +0300

    final commit, added menu to start or quit game

--- hangman.py
+++ hangman.py
@@ -16,42 +16,50 @@ print("H A N G M A N\n")
 
 chosen_letter = set()
 tries = 8
-while tries > 0:
-    answer_so_far = "".join(dash_list)
-
-    print(answer_so_far)
-    if answer_so_far == random_choice:
-        print("You guessed the word")
-        print("You survived!")
+while True:
+    print('Type "play" to play the game, "exit" to quit: ')
+    decision = input()
+    if decision == 'exit':
         break
+    elif decision == 'play':
+        while tries > 0:
+            answer_so_far = "".join(dash_list)
 
-    letter = input("Input a letter: ")
-    if len(letter) != 1:
-        print("You should input a single letter\n")
-    else:
-        if letter not in ascii_lowercase:
-            print("It is not an ASCII lowercase letter\n")
+            print(answer_so_far)
+            if answer_so_far == random_choice:
+                print("You guessed the word")
+                print("You survived!")
+                break
 
-        else:
-            if letter in random_choice:
-                if letter not in chosen_letter:
-                    print()
-                    for i in range(0, num):
-                        if random_choice[i] == letter:
-                            dash_list[i] = random_choice[i]
-                            chosen_letter.add(letter)
-
-                else:
-                    print("You already typed this letter\n")
-
+            letter = input("Input a letter: ")
+            if len(letter) != 1:
+                print("You should input a single letter\n")
             else:
-                if letter in chosen_letter:
-                    print("You already typed this letter\n")
+                if letter not in ascii_lowercase:
+                    print("It is not an ASCII lowercase letter\n")
+
                 else:
-                    print("No such letter in the word")
-                    chosen_letter.add(letter)
-                    tries -= 1
-                    if tries == 0:
-                        print("You are hanged!")
+                    if letter in random_choice:
+                        if letter not in chosen_letter:
+                            print()
+                            for i in range(0, num):
+                                if random_choice[i] == letter:
+                                    dash_list[i] = random_choice[i]
+                                    chosen_letter.add(letter)
+
+                        else:
+                            print("You already typed this letter\n")
+
                     else:
-                        print()
+                        if letter in chosen_letter:
+                            print("You already typed this letter\n")
+                        else:
+                            print("No such letter in the word")
+                            chosen_letter.add(letter)
+                            tries -= 1
+                            if tries == 0:
+                                print("You are hanged!")
+                            else:
+                                print()
+    else:
+        print("Unknown input")
